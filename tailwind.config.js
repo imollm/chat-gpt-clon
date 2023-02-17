361dd8677e749109d429717e1ee22b557a4512d1
Ivan Moll <ivanmoll07@gmail.com>
2023-02-17 01:20:08 +0100

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      animation: {
+        typing: 'blink 1s steps(5, start) infinite'
+      },
+      blink: {
+        to: {
+          visibility: 'hidden'
+        }
+      },
+      backgroundColor: {
+        gptlogo: '#10a37f',
+        gptdarkgray: '#202123',
+        gptgray: '#343541',
+        gptlightgray: '#444654'
+      },
+      colors: {
+        gptgray: '#8d8d8d'
+      }
+    },
+  },
+  plugins: [],
+}
