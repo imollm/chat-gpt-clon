e7ae33cb0c03dea9cbf4cc4873ec8044f926e5ed
Ivan Moll <ivanmoll07@gmail.com>
2023-02-24 11:50:54 +0100

    Add supabase class to manage supbase things

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,102 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseApiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+class SupabaseClient {
+	
+	constructor() {
+		this.supabaseUrl = process.env.SUPABASE_URL
+		this.supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+		this.supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
+		this.options = {
+			global: {
+				headers: {
+					'Authorization': `Bearer ${this.supabaseSecretKey}`
+				}
+			}
+		}
+		this.client = createClient(
+			this.supabaseUrl,
+			this.supabaseAnonKey,
+			this.options
+		)
+	}
 
-export const supabase = createClient(
-	supabaseUrl,
-	supabaseApiKey
-)
+	static getInstance() {
+		if (!this.instance) {
+			this.instance = new SupabaseClient()
+		}
+
+		return this.instance
+	}
+
+	async storeChat(chat) {
+		const { data, error }
+			= await this.client
+				.from('chats').insert({
+					...chat
+				})
+				.select()
+
+		if (error) throw new Error(error)
+
+		return data
+	}
+
+	async getChat(chatId) {
+		const { data, error }
+			= await this.client
+				.from('chats')
+				.select()
+				.eq('id', chatId)
+
+		return data
+	}
+
+	async getChats(userId) {
+		const { data, error }
+			= await this.client
+				.from('chats')
+				.select()
+				.eq('user_id', userId)
+
+		return data
+	}
+
+	async getLastChat(userId) {
+		const { data, error }
+			= await this.client
+				.from('chats')
+				.select()
+				.eq('user_id', userId)
+				.order('created_at', { ascending: false })
+				.limit(1)
+
+		return data
+	}
+
+	async getMessages(chatId) {
+		const { data, error }
+			= await this.client
+				.from('messages')
+				.select()
+				.eq('chat_id', chatId)
+
+		return data
+	}
+
+	async storeMessage(message, chatId) {
+		const { data, error }
+			= await this.client
+				.from('messages')
+				.insert({
+					...message,
+					chat_id: chatId
+				})
+				.select()
+
+		if (error) throw new Error(error)
+
+		return data
+	}
+}
+
+export default SupabaseClient
