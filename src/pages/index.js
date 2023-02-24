39fa6f25cc93dd093a007b7d9e45b28ddd1ae043
Ivan Moll <ivanmoll07@gmail.com>
2023-02-24 16:40:55 +0100

    Change login component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,22 @@
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Layout } from '@/components/Layout'
 import { Chat } from '@/components/Chat'
+import { Login } from '@/components/Login'
 
 const Home = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
 
   return (
-    <div className='container' style={{ padding: '50px 0 100px 0' }}>
+    <>
       {!session ? (
-        <Auth
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          theme='dark'
-          providers={['github']}
-        />
+        <Login supabase={supabase} />
       ) : (
         <Layout>
           <Chat session={session} />
         </Layout>
       )}
-    </div>
+    </>
   )
 }
 
