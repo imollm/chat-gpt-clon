9a8e85a302f7140ad8616f6e3d6690576f16bea8
Ivan Moll <ivanmoll07@gmail.com>
2023-03-16 00:10:41 +0100

    Fix bottom of messages container

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -97,7 +97,7 @@ export function Chat({ session }) {
 
   return (
     <div className='relative flex flex-col flex-1 h-full ml-64'>
-      <main className='flex flex-col items-center gap-3 h-[calc(100% - 150px)] overflow-y-scroll'>
+      <main className='flex flex-col items-center gap-3 mb-[150px] overflow-y-scroll'>
         {loading && (
           <div className='pt-6'>
             <Spinner />
